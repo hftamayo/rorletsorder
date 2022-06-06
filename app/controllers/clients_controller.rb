187e8674f9ce37686703329ff02d6eac44e40661
hftamayo <hftamayo@gmail.com>
2022-06-06 09:37:28 -0600

    updating clients controller

--- app/controllers/clients_controller.rb
+++ app/controllers/clients_controller.rb
@@ -1,2 +1,52 @@
 class ClientsController < ApplicationController
+        #GET /clients
+        def index
+            @clients = Client.all
+            render json: @clients
+        end
+    
+        #GET /client/:id
+        def show
+            render json: @client
+        end
+    
+            @client = Client.find(params[:id])
+        #POST /clients
+        def create
+            @client = Cient.new(client_params)
+            if @client.save
+                render json: @client
+            else
+                render error: { error: 'Unable to signup the client'}, status: 400
+            end
+        end
+    
+        #PUT /clients/:id
+        def update
+            @client = Client.find(params[:id])
+            if @client
+                @client.update(client_params)
+                render json: { message: 'Client successfully updated.'}, status: 200
+            else
+                render json: { error: 'Unable to update client data.', status: 400}
+            end
+        end
+    
+    
+        #DELETE /clients/:id
+        def destroy
+            @client = Client.find(params[:id])
+            if @client
+                @client.destroy
+                render json: { message: 'Client successfully deleted.'}, status: 200
+            else
+                render json: { error: 'Unable to delete Client.'}, status: 400
+            end
+        end
+    
+        private
+    
+        def client_params
+            params.require(:client).permit(:firstname, :lastname, :email, :password_digest)
+        end
 end
