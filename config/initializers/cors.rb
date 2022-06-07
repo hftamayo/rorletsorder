eb428d8f1aff118ba16c7a320eee04bcdb1ee391
hftamayo <hftamayo@gmail.com>
2022-06-07 11:19:40 -0600

    tries for solving cors response

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,10 +7,8 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'http://localhost:3005/'
+    origins 'localhost:3005', '127.0.0.1:3005'
 
-    resource '*',
-      headers: :any,
-      methods: [:get, :post, :put, :patch, :delete, :options, :head]
+    resource '*', headers: :any, methods: [:get, :post, :put, :patch, :delete, :options, :head]
   end
 end
