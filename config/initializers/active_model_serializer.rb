c420cb136f7c15a99657a42581c8035d2463f6b2
hftamayo <hftamayo@gmail.com>
2022-06-08 11:27:28 -0600

    commenting active model serializer

--- config/initializers/active_model_serializer.rb
+++ config/initializers/active_model_serializer.rb
@@ -1,9 +1,9 @@
-ActiveModelSerializers.config.adapter = :json_api
+# ActiveModelSerializers.config.adapter = :json_api
 
-#this support POST request
-api_mime_types = %W(
-  application/vnd.api+json
-  text/x-json
-  application/json
-)
-Mime::Type.register 'application/vnd.api+json', :json, api_mime_types
+# this support POST request
+# api_mime_types = %W(
+#   application/vnd.api+json
+#   text/x-json
+#   application/json
+# )
+# Mime::Type.register 'application/vnd.api+json', :json, api_mime_types
