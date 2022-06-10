cf471d1c9505eacf4577a611740eec42ae88e55b
hftamayo <hftamayo@gmail.com>
2022-06-10 13:47:12 -0600

    removing mealid from ordertemp schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_06_10_193541) do
+ActiveRecord::Schema.define(version: 2022_06_10_194432) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -57,7 +57,6 @@ ActiveRecord::Schema.define(version: 2022_06_10_193541) do
 
   create_table "ordertemps", force: :cascade do |t|
     t.integer "amount"
-    t.integer "mealid"
     t.string "name"
     t.string "price"
     t.datetime "created_at", precision: 6, null: false
