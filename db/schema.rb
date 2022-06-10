b91e697ac892462443dddd253d6ccdf0ee49c836
hftamayo <hftamayo@gmail.com>
2022-06-10 13:43:07 -0600

    updating price datatype

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_06_09_185927) do
+ActiveRecord::Schema.define(version: 2022_06_10_193541) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -59,7 +59,7 @@ ActiveRecord::Schema.define(version: 2022_06_09_185927) do
     t.integer "amount"
     t.integer "mealid"
     t.string "name"
-    t.decimal "price"
+    t.string "price"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.string "clientName"
