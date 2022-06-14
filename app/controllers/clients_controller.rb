531328a8718842bd6588657271f9907a8e10c4ae
hftamayo <hftamayo@gmail.com>
2022-06-14 12:10:37 -0600

    adjusting methods on client controller

--- app/controllers/clients_controller.rb
+++ app/controllers/clients_controller.rb
@@ -1,23 +1,26 @@
 class ClientsController < ApplicationController
+    before_action :authorize_request, except: :create
+    before_action :find_client, except: %i[create index]
+
         #GET /clients
         def index
             @clients = Client.all
-            render json: @clients
+            render json: @clients, status: :ok
         end
     
         #GET /client/:id
         def show
             @client = Client.find(params[:id])
-            render json: @client
+            render json: @client, status: :ok
         end
     
         #POST /clients
         def create
             @client = Client.new(client_params)
             if @client.save
-                render json: @client
+                render json: @client, status: :created
             else
-                render error: { error: 'Unable to signup the client'}, status: 400
+                render json: { errors: @client.errors.full_messages }, status: 400
             end
         end
     
@@ -28,7 +31,7 @@ class ClientsController < ApplicationController
                 @client.update(client_params)
                 render json: { message: 'Client successfully updated.'}, status: 200
             else
-                render json: { error: 'Unable to update client data.', status: 400}
+                render json: { errors: @client.errors.full_messages }, status: :unprocessable_entity
             end
         end
     
@@ -43,6 +46,12 @@ class ClientsController < ApplicationController
                 render json: { error: 'Unable to delete Client.'}, status: 400
             end
         end
+
+        def find_client
+            @client = Client.find_by_email!(params[:_email])
+            rescue ActiveRecord::RecordNotFound
+              render json: { errors: 'User not found' }, status: :not_found
+          end        
     
         private
     
