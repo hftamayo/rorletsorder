8e2d53b864e140f4b396152553c8c22e1fd9097d
hftamayo <hftamayo@gmail.com>
2022-06-16 14:24:37 -0600

    updates on auth controller

--- app/controllers/authentication_controller.rb
+++ app/controllers/authentication_controller.rb
@@ -1,22 +1,30 @@
 class AuthenticationController < ApplicationController
-    before_action :authorize_request, except: :login
+    #before_action :authorize_request, except: :login
+    skip_before_action :verify_authenticity_token, :only => :authenticate
+    skip_before_action :authorize_request, only: :authenticate, raise: false
 
-  # POST /auth/login
-  def login
-    @client = Client.find_by_email(params[:email])
-    if @client&.authenticate(params[:password_digest])
-      token = JsonWebToken.encode(client_id: @client.id)
-      time = Time.now + 24.hours.to_i
-      render json: { token: token, exp: time.strftime("%m-%d-%Y %H:%M"),
-                     email: @client.email }, status: :ok
-    else
-      render json: { error: 'unauthorized' }, status: :unauthorized
+    #/signin
+    def authenticate
+      auth_token = AuthenticateClient.new(auth_params[:email], auth_params[:password]).call
+      json_response(auth_token: auth_token)
     end
-  end
+
+    # POST /auth/login
+    def login
+      @client = Client.find_by_email(params[:email])
+      if @client&.authenticate(params[:password_digest])
+        token = JsonWebToken.encode(client_id: @client.id)
+        time = Time.now + 24.hours.to_i
+        render json: { token: token, exp: time.strftime("%m-%d-%Y %H:%M"),
+                      email: @client.email }, status: :ok
+      else
+        render json: { error: 'unauthorized' }, status: :unauthorized
+      end
+    end
 
   private
 
   def login_params
-    params.permit(:email, :password_digest)
+    params.permit(:email, :password)
   end
 end
