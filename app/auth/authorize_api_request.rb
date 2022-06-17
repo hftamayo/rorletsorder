41f0affd3e6c7f2588160d354d09dd8e9f47c745
hftamayo <hftamayo@gmail.com>
2022-06-17 12:32:21 -0600

    finishing authorize api request

--- app/auth/authorize_api_request.rb
+++ app/auth/authorize_api_request.rb
@@ -14,5 +14,30 @@ class AuthorizeApiRequest
 
     attr_reader :headers
 
-    
+    def user
+        #verify if the client is in the database
+        #load object in memory
+        @client ||= Client.find(decoded_auth_token[:client_id]) if decoded_auth_token
+        #handler for client not found
+    rescue ActiveRecord::RecordNotFound => e
+        raise(
+            ExceptionHandler::InvalidToken,
+            ("#{Message.invalid_token} #{e.message}")
+        )
+    end
+
+    #decoding auth token
+    def decoded_auth_token
+        @decoded_auth_token ||= JsonWebToken.decode(http_auth_header)
+    end
+
+    #checking for token in authorization header
+    def http_auth_header
+        if headers['Authorization'].present?
+            return headers['Authorization'].split(' ').last
+        end
+        raise(ExceptionHandler::MissingToken.Message.missing_token)
+    end
+
+
 end
