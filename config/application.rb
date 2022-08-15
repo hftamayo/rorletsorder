2af2f0d0db5d90d745025550a0a77af974e0018c
hftamayo <hftamayo@gmail.com>
2022-08-15 13:04:35 -0600

    store jwt token: application.rb

--- config/application.rb
+++ config/application.rb
@@ -24,6 +24,10 @@ module Rorletsorder
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.1
 
+    #hftamayo = store jwt token
+    config.middleware.use ActionDispatch::Cookies
+    config.middleware.use ActionDispatch::Session::CookieStore
+
     # Configuration for the application, engines, and railties goes here.
     #
     # These settings can be overridden in specific environments using the files
@@ -35,6 +39,6 @@ module Rorletsorder
     # Only loads a smaller set of middleware suitable for API only apps.
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
-    config.api_only = true
+    config.api_only = false
   end
 end
